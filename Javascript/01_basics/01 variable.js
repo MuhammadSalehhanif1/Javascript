b4e6277dc5f3c03550b5617b5721711f1709a129
MuhammadSalehhanif1 <m.salehhanif@gmail.com>
2026-09-10 20:16:38 +0500

    Javascript basic

---
+++ Javascript/01_basics/01 variable.js
@@ -0,0 +1,12 @@
+const accountId=1
+let accountEmail="test@google.com"
+var accountName="tester"
+accountCity="karachi"
+
+// accountId=02 // is not allowed 
+
+accountEmail="tester@google.com"
+accountName="test"
+accountCity="islamabad"
+
+console.table([accountId,accountName,accountEmail,accountCity])
