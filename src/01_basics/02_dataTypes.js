4755691609dc358a150baae09e08ecd7a1f33b26
MuhammadSalehhanif1 <m.salehhanif@gmail.com>
2026-09-11 22:57:43 +0500

    Data Type

---
+++ src/01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict";// treat all js code as a new version
+// alert()// alert is browser part not node and we using node js
+
+console.log(3 
+    +
+    3
+);// code humesha readabale hona chaye hy
+
+//data types in js
+// number
+// string=> ""
+//bigint=> for Big number like using in big scale no
+// Boolean => true/ false
+// null => standlone value
+// undefined
+// symbol => for unique 
+// object
+
+let name="abid"
+let age=20
+ 
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof null);// output is  object
+console.log(typeof undefined); // output is undefined
+
+
+
