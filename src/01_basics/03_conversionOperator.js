5ae967da9123fac52276cc74c2508417e940e2aa
MuhammadSalehhanif1 <m.salehhanif@gmail.com>
2026-09-14 16:33:12 +0500

    conversion Operator

---
+++ src/01_basics/03_conversionOperator.js
@@ -0,0 +1,37 @@
+let score=false
+console.log(typeof (score));
+console.log(typeof score);
+
+let valueChange=Number(score)
+console.log(typeof valueChange);
+console.log(valueChange);
+
+// NaN Ki type number
+// "33" => Nan
+//"33abc" => Nan
+// true => 1 false => 0
+
+// how to change value in boolean
+
+let value=function(){}
+let BooleanisLoggedIn=Boolean(value)
+console.log(BooleanisLoggedIn);
+ 
+// "0" in strange => true
+// 0 => false 1=> true 
+// "abid" => true
+// function is true value
+// so there  are six to seven type of false value
+
+
+// how to change valuein String
+let text=null
+let StringIsLoggedIn=String(text)
+console.log(StringIsLoggedIn);
+console.log(typeof StringIsLoggedIn);// 124 => type is string
+
+
+// iska faida ke bohot bar value ko change karna hota number to string
+// and string to number so ye method bohot useful hotay hy
+
+
