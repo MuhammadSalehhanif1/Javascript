2405bbaaf22df025917a3a5ac56c564d5b2dbaec
MuhammadSalehhanif1 <m.salehhanif@gmail.com>
2026-09-15 22:44:38 +0500

    string to number conversion

--- src/01_basics/03_conversionOperator.js
+++ src/01_basics/03_conversionOperator.js
@@ -1,10 +1,10 @@
 let score=false
-console.log(typeof (score));
-console.log(typeof score);
+// console.log(typeof (score));
+// console.log(typeof score);
 
 let valueChange=Number(score)
-console.log(typeof valueChange);
-console.log(valueChange);
+// console.log(typeof valueChange);
+// console.log(valueChange);
 
 // NaN Ki type number
 // "33" => Nan
@@ -15,7 +15,7 @@ console.log(valueChange);
 
 let value=function(){}
 let BooleanisLoggedIn=Boolean(value)
-console.log(BooleanisLoggedIn);
+// console.log(BooleanisLoggedIn);
  
 // "0" in strange => true
 // 0 => false 1=> true 
@@ -27,11 +27,55 @@ console.log(BooleanisLoggedIn);
 // how to change valuein String
 let text=null
 let StringIsLoggedIn=String(text)
-console.log(StringIsLoggedIn);
-console.log(typeof StringIsLoggedIn);// 124 => type is string
+// console.log(StringIsLoggedIn);
+// console.log(typeof StringIsLoggedIn);// 124 => type is string
 
 
 // iska faida ke bohot bar value ko change karna hota number to string
 // and string to number so ye method bohot useful hotay hy
+
+
+// ***************** Operation ****************************//
+
+let no=3
+let view=-no
+console.log(view)// -3 output
+
+let str1="hello"
+let str2=" abid"
+let str3=str1+str2
+// console.log(str3)//hello abid Output
+
+// console.log(1+1); // 2 
+// console.log(1-1); //0 
+// console.log(2*2); //4
+// console.log(2/3); //0.66
+// console.log(2**3);//8
+// console.log(2%3); //2
+
+// console.log("2"+2); //22
+// console.log(2+"2"); //22
+// console.log("2"+2+2+2); //2222
+// console.log(2+2+2+"2"); //62
+// console.log("2"+(2+2+2)); //26
+
+// "simple rule confusion code likha kyu hy : aesa code likhe jisme samaj aye(readable ho) "
+
+let gamecounter=0
+gamecounter++
+console.log(gamecounter);
+
+console.log(+true);// 1
+console.log(+"");// 0
+
+
+
+
+
+
+
+
+ 
+
 
 
