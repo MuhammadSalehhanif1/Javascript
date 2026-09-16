f4940e83de4f088edc2597f637c3929c974b3aa4
MuhammadSalehhanif1 <m.salehhanif@gmail.com>
2026-09-16 22:04:40 +0500

    comparison of datatypes

---
+++ src/01_basics/04_comparision.js
@@ -0,0 +1,44 @@
+// console.log(2==2);//true
+// console.log(2>2);//false
+// console.log(2>=2);//true
+// console.log(2<=2);//true
+// console.log(2==2);//true
+// console.log(2!=2);//false
+
+// console.log(null == 0);//false
+// console.log(null <=0);// true
+// console.log(null >=0);//true
+  
+// console.log("2"==2);//true
+// console.log("2">=2);//true
+  
+
+console.log(undefined == true);// undefined me humesha false hy ata hy
+console.log(undefined == 0);//true
+console.log(undefined > 0);//true
+console.log(undefined < 0);//true
+   
+
+// upar jitna code likha hy isko likhne se avaoid karna chaye hy
+// isliye ke code readable and clean hona chaye hy 
+// and kyu itni confusion karne hy
+
+
+// ************** strict check ************///
+/// mean === ye data type ko bah check karta hy
+
+console.log(2=="2");//true
+console.log("2"===2);//false bcz data type is not equal
+
+
+
+
+
+
+
+
+
+
+
+
+
